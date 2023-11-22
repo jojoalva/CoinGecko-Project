05a35e223d11bbd4242ebed48dfeb54f8da7e3f5
jojoalva <140528886+jojoalva@users.noreply.github.com>
2023-11-22 22:08:43 +0000

    nev_and_jo_code

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def welcome():
         "Available Routes:<br/>"
         '<a href="/api/metadata">/api/metadata</a><br/>'
         '<a href="/metadata">/html/metadata</a><br/>'
-        '<a href="/other_route">/other_route</a><br/>'
+        '<a href="/dashboard">/html/dashboard</a><br/>'
     )
 
 #################################################
@@ -47,22 +47,39 @@ def extract():
     session = Session(engine)
 
     # Query data we want
-    results = session.query(Coins.name, Coins.current_price, Coins.market_cap, Coins.total_volume, 
-                            Coins.high_24h, Coins.low_24h).all()
+    results = session.query(Coins).all()
 
     session.close()
 
      # Create a dictionary from the row data and append to a list of all_data
     all_data = []
-    for name, current_price, market_cap, total_volume, high_24h, low_24h in results:
-        data_dict = {}
-        data_dict["name"] = name
-        data_dict["current_price"] = current_price
-        data_dict["market_cap"] = market_cap
-        data_dict["total_volume"] = total_volume
-        data_dict["high_24h"] = high_24h
-        data_dict["low_24h"] = low_24h
-        all_data.append(data_dict)
+    for result in results:
+        all_data.append({
+            "id": result.id,
+            "name": result.name,
+            "symbol": result.symbol,
+            "current_price": result.current_price,
+            "market_cap": result.market_cap,
+            "market_cap_rank": result.market_cap_rank,
+            "fully_diluted_valuation": result.fully_diluted_valuation,
+            "total_volume": result.total_volume,
+            "high_24h": result.high_24h,
+            "low_24h": result.low_24h,
+            "price_change_24h": result.price_change_24h,
+            "price_change_percentage_24h": result.price_change_percentage_24h,
+            "market_cap_change_24h": result.market_cap_change_24h,
+            "market_cap_change_percentage_24h": result.market_cap_change_percentage_24h,
+            "circulating_supply": result.circulating_supply,
+            "total_supply": result.total_supply,
+            "max_supply": result.max_supply,
+            "ath": result.ath,
+            "ath_change_percentage": result.ath_change_percentage,
+            "ath_date": result.ath_date,
+            "atl": result.atl,
+            "atl_change_percentage": result.atl_change_percentage,
+            "atl_date": result.atl_date,
+            "last_updated": result.last_updated
+        })
 
     # Save the JSON File
     file2 = open('static/data/all_dataJSON.json', 'w')
@@ -79,6 +96,55 @@ def extract():
 def metadata():
 
     return render_template('index.html')
+
+@app.route("/dashboard")
+def Dashboard():
+    session = Session(engine)
+
+    # Query the Coins table and retrieve some data
+    results = session.query(Coins).all()
+
+    session.close()
+
+    # Convert the query results to a list of dictionaries
+    Crypto_data = []
+    for result in results:
+        Crypto_data.append({
+            "id": result.id,
+            "name": result.name,
+            "symbol": result.symbol,
+            "current_price": result.current_price,
+            "market_cap": result.market_cap,
+            "market_cap_rank": result.market_cap_rank,
+            "fully_diluted_valuation": result.fully_diluted_valuation,
+            "total_volume": result.total_volume,
+            "high_24h": result.high_24h,
+            "low_24h": result.low_24h,
+            "price_change_24h": result.price_change_24h,
+            "price_change_percentage_24h": result.price_change_percentage_24h,
+            "market_cap_change_24h": result.market_cap_change_24h,
+            "market_cap_change_percentage_24h": result.market_cap_change_percentage_24h,
+            "circulating_supply": result.circulating_supply,
+            "total_supply": result.total_supply,
+            "max_supply": result.max_supply,
+            "ath": result.ath,
+            "ath_change_percentage": result.ath_change_percentage,
+            "ath_date": result.ath_date,
+            "atl": result.atl,
+            "atl_change_percentage": result.atl_change_percentage,
+            "atl_date": result.atl_date,
+            "last_updated": result.last_updated
+        })
+
+    # Sort Crypto_data based on market_cap in descending order
+    sorted_data = sorted(Crypto_data, key=lambda x: x['market_cap'], reverse=True)
+
+    # Extract top 10 cryptocurrencies based on market dominance
+    top_10_currencies = sorted_data[:10]
+
+    print(top_10_currencies)
+
+    return render_template('dashboard.html', top_10_currencies=top_10_currencies) 
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/crypto_metadata.js
+++ static/js/crypto_metadata.js
@@ -1,16 +1,27 @@
 d3.json("static/data/all_dataJSON.json").then((data) => {
   console.log(data);
 
-  function updateMetaData(subjectMetadata) {
-  
-    let demoBox = d3.select("#sample-metadata");
-  
-    // Clear the existing content
-    demoBox.html("")
-  
-  // Iterate through the metadata object and append each key-value pair
-  Object.entries(subjectMetadata).forEach(([key, value]) => {
-    demoBox.append("p").text(`${key}: ${value}`);
+  // Select all buttons and attach a click event handler
+  d3.selectAll("button").on("click", function() {
+    // Inside the click event handler function
+    let input = document.getElementById('search').value.toLowerCase();
+    let selectedCoinData = data.find(item => item.name.toLowerCase() === input);
+    
+    // Call the updateMetaData function with the selected data
+    updateMetaData(selectedCoinData);
   });
-  
+
+  // Define the updateMetaData function outside the event handler
+  function updateMetaData(subjectMetadata) {
+    let demoBox = d3.select("#sample-metadata");
+    demoBox.html(""); // Clear the existing content
+
+    if (subjectMetadata) {
+      Object.entries(subjectMetadata).forEach(([key, value]) => {
+        demoBox.append("p").text(`${key}: ${value}`);
+      });
+    } else {
+      demoBox.append("p").text("No data found for the selected cryptocurrency.");
+    }
   }
+});
